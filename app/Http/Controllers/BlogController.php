21d8d91326cd241faa829f9941fd840e14e9b97f
LudoPilot <e.berardfr@gmail.com>
2025-11-23 15:19:03 +0100

    Add methods to BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+	public function index()
+	{
+		Article::all();
+
+		return inertia('Blog/');
+	}
+
+    public function create()
+	{
+		return inertia('Blog/Create');
+	}
+
+	public function store(Request $request) {
+		$validated = $request->valdiate([
+			'title' => 'required|string|max:255',
+			'content' => 'required|string'
+		]);
+
+		Article::create([
+			'title' => $validated['title'],
+			'content' => $validated['content'],
+			'user_id' => auth()->id(),
+		]);
+
+		return redirect()->route('blog.index')->with('success', 'Article créé avec succès.');
+	}
+
+	public function show(Article $article) {
+		return inertia('Blog/Show', [
+			'article' => $article
+		]);
+	} 
+
+	public function edit(Article $article) {
+		return inertia('Blog/Edit', [
+			'article' => $article
+		]);
+	}
+
+	public function update(Request $request, Article $article) {
+		$validated = $request->validate([
+			'title' => 'required|string|max:255',
+			'content' => 'required|string'
+		]);
+
+		$article->update($validated);
+
+		// retourner à la liste des articles
+        return redirect()->route('blog.index')
+            ->with('success', 'Article mis à jour.');
+	}
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return redirect()->route('blog.index')
+            ->with('success', 'Article supprimé.');
+    }
 }
