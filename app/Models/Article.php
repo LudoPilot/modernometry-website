729bfdf5df923f442d52ec7d4c33ee6415889b41
LudoPilot <e.berardfr@gmail.com>
2025-11-26 23:37:56 +0100

    Ajout des modèles Category et Tag. Mise à jour du modèle Article.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -13,10 +13,22 @@ class Article extends Model
 		'title',
 		'content',
 		'user_id',
+		'category_id',
 	];
 
 	public function user()
 	{
 		return $this->belongsTo(User::class);
 	}
+
+	public function category()
+	{
+		return $this->belongsTo(Category::class);
+	}
+
+	public function tags()
+	{
+		return $this->belongsToMany(Tag::class);
+	}
+
 }
