17267ff21cbe77d111d4a4242244fcedb091b9f5
LudoPilot <e.berardfr@gmail.com>
2025-11-27 14:28:32 +0100

    Récupération du slug pour Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -16,6 +17,30 @@ class Article extends Model
 		'category_id',
 	];
 
+	public function setTitleAttribute($value)
+	{
+		$this->attributes['title'] = $value;
+
+		$baseSlug = Str::slug($value);
+		$slug = $baseSlug;
+		$count = 1;
+
+		// vérifie que le slug est unique
+		while (static::where('slug', $slug)->exists()) {
+			$slug = $baseSlug . '-' . $count++;
+		}
+
+		$this->attributes['slug'] = $slug;
+	}
+
+	// récupérer le slug
+	public function getRouteKeyName()
+	{
+		return 'slug';
+	}
+
+
+	// RELATIONS
 	public function user()
 	{
 		return $this->belongsTo(User::class);
